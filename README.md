61308d2f14f92defccd608056fcd0265b37f7d13
bighead-sh <58314765+bighead-sh@users.noreply.github.com>
2020-06-26 11:04:49 +0200

    cloud9

---
+++ README.md
@@ -0,0 +1,5 @@
+# cloud9
+
+```
+curl -L https://raw.githubusercontent.com/bighead-sh/open/master/cloud9.sh | sudo sh
+```
---
+++ cloud9.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Enter cloud9 port: "  
+read PORT  
+
+
+echo "Enter password for admin user: "  
+read PASSWORD  
+
+
+apt install build-essential python -y
+
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
+
+curl -L https://npmjs.org/install.sh | sudo sh
+
+
+git clone https://github.com/c9/core sdk
+cd sdk/
+./scripts/install-sdk.sh
+
+npm i forever -g
+
+cat << EOF > /etc/init.d/vpsstart.sh 
+
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:          	vps
+# Required-Start:     	\$all
+# Required-Stop:      	\$all
+# Default-Start:     	2 3 4 5
+# Default-Stop:      	0 1 6
+# Short-Description:    vpsstart
+# Description:       	Enable service provided by daemon.
+### END INIT INFO
+
+su -c "cd /home/ & forever start /root/sdk/server.js -w / -p $PORT -l 0.0.0.0  -a admin:$PASSWORD --collab" root;
+
+EOF
+
+chmod 777 /etc/init.d/vpsstart.sh 
+update-rc.d vpsstart.sh defaults
